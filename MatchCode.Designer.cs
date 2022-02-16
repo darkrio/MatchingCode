8132585103c0a7f20683cac9a1afb96a0d0c224d
Richard Chang <darkrio@qq.com>
2022-02-16 10:02:22 +0800

    添加项目文件。

---
+++ MatchCode.Designer.cs
@@ -0,0 +1,170 @@
+﻿
+namespace MatchingCode.UI
+{
+    partial class MatchCode
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtLabel1 = new System.Windows.Forms.Label();
+            this.txtLabel2 = new System.Windows.Forms.Label();
+            this.tbxExcelD = new System.Windows.Forms.TextBox();
+            this.tbxExcelC = new System.Windows.Forms.TextBox();
+            this.rtbLogs = new System.Windows.Forms.RichTextBox();
+            this.btnExe = new System.Windows.Forms.Button();
+            this.btnOpen1 = new System.Windows.Forms.Button();
+            this.btnOpen2 = new System.Windows.Forms.Button();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.SuspendLayout();
+            // 
+            // txtLabel1
+            // 
+            this.txtLabel1.AutoSize = true;
+            this.txtLabel1.Font = new System.Drawing.Font("Microsoft YaHei UI", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.txtLabel1.Location = new System.Drawing.Point(9, 8);
+            this.txtLabel1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.txtLabel1.Name = "txtLabel1";
+            this.txtLabel1.Size = new System.Drawing.Size(210, 25);
+            this.txtLabel1.TabIndex = 0;
+            this.txtLabel1.Text = "病例组Excel文件路径：";
+            // 
+            // txtLabel2
+            // 
+            this.txtLabel2.AutoSize = true;
+            this.txtLabel2.Font = new System.Drawing.Font("Microsoft YaHei UI", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.txtLabel2.Location = new System.Drawing.Point(9, 42);
+            this.txtLabel2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.txtLabel2.Name = "txtLabel2";
+            this.txtLabel2.Size = new System.Drawing.Size(210, 25);
+            this.txtLabel2.TabIndex = 1;
+            this.txtLabel2.Text = "对照组Excel文件路径：";
+            // 
+            // tbxExcelD
+            // 
+            this.tbxExcelD.Location = new System.Drawing.Point(199, 8);
+            this.tbxExcelD.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.tbxExcelD.Name = "tbxExcelD";
+            this.tbxExcelD.Size = new System.Drawing.Size(293, 23);
+            this.tbxExcelD.TabIndex = 2;
+            this.tbxExcelD.TextChanged += new System.EventHandler(this.tbxExcelD_TextChanged);
+            // 
+            // tbxExcelC
+            // 
+            this.tbxExcelC.Location = new System.Drawing.Point(199, 42);
+            this.tbxExcelC.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.tbxExcelC.Name = "tbxExcelC";
+            this.tbxExcelC.Size = new System.Drawing.Size(293, 23);
+            this.tbxExcelC.TabIndex = 3;
+            this.tbxExcelC.TextChanged += new System.EventHandler(this.tbxExcelC_TextChanged);
+            // 
+            // rtbLogs
+            // 
+            this.rtbLogs.Location = new System.Drawing.Point(9, 70);
+            this.rtbLogs.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.rtbLogs.Name = "rtbLogs";
+            this.rtbLogs.Size = new System.Drawing.Size(580, 268);
+            this.rtbLogs.TabIndex = 4;
+            this.rtbLogs.Text = "";
+            this.rtbLogs.TextChanged += new System.EventHandler(this.rtbLogs_TextChanged);
+            // 
+            // btnExe
+            // 
+            this.btnExe.Font = new System.Drawing.Font("Microsoft YaHei UI", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btnExe.Location = new System.Drawing.Point(499, 342);
+            this.btnExe.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.btnExe.Name = "btnExe";
+            this.btnExe.Size = new System.Drawing.Size(89, 31);
+            this.btnExe.TabIndex = 5;
+            this.btnExe.Text = "执行";
+            this.btnExe.UseVisualStyleBackColor = true;
+            this.btnExe.Click += new System.EventHandler(this.btnExe_Click);
+            // 
+            // btnOpen1
+            // 
+            this.btnOpen1.Location = new System.Drawing.Point(499, 6);
+            this.btnOpen1.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.btnOpen1.Name = "btnOpen1";
+            this.btnOpen1.Size = new System.Drawing.Size(73, 25);
+            this.btnOpen1.TabIndex = 6;
+            this.btnOpen1.Text = "加载";
+            this.btnOpen1.UseVisualStyleBackColor = true;
+            this.btnOpen1.Click += new System.EventHandler(this.btnOpen1_Click);
+            // 
+            // btnOpen2
+            // 
+            this.btnOpen2.Location = new System.Drawing.Point(499, 40);
+            this.btnOpen2.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.btnOpen2.Name = "btnOpen2";
+            this.btnOpen2.Size = new System.Drawing.Size(73, 25);
+            this.btnOpen2.TabIndex = 7;
+            this.btnOpen2.Text = "加载";
+            this.btnOpen2.UseVisualStyleBackColor = true;
+            this.btnOpen2.Click += new System.EventHandler(this.btnOpen2_Click);
+            // 
+            // backgroundWorker1
+            // 
+            this.backgroundWorker1.WorkerReportsProgress = true;
+            this.backgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker1_DoWork);
+            this.backgroundWorker1.ProgressChanged += new System.ComponentModel.ProgressChangedEventHandler(this.backgroundWorker1_ProgressChanged);
+            this.backgroundWorker1.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.backgroundWorker1_RunWorkerCompleted);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 17F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(600, 382);
+            this.Controls.Add(this.btnOpen2);
+            this.Controls.Add(this.btnOpen1);
+            this.Controls.Add(this.btnExe);
+            this.Controls.Add(this.rtbLogs);
+            this.Controls.Add(this.tbxExcelC);
+            this.Controls.Add(this.tbxExcelD);
+            this.Controls.Add(this.txtLabel2);
+            this.Controls.Add(this.txtLabel1);
+            this.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "MatchingProgram";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label txtLabel1;
+        private System.Windows.Forms.Label txtLabel2;
+        private System.Windows.Forms.TextBox tbxExcelD;
+        private System.Windows.Forms.TextBox tbxExcelC;
+        private System.Windows.Forms.RichTextBox rtbLogs;
+        private System.Windows.Forms.Button btnExe;
+        private System.Windows.Forms.Button btnOpen1;
+        private System.Windows.Forms.Button btnOpen2;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+    }
+}
+
---
+++ MatchCode.cs
@@ -0,0 +1,258 @@
+﻿using System;
+using System.ComponentModel;
+using System.Data;
+using System.Linq;
+using System.Windows.Forms;
+using Aspose.Cells;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace MatchingCode.UI
+{
+    public partial class MatchCode : Form
+    {
+        private string exceld = "";
+        private string excelc = "";
+
+        public MatchCode()
+        {
+            System.Text.Encoding.RegisterProvider(System.Text.CodePagesEncodingProvider.Instance);
+            InitializeComponent();
+            this.rtbLogs.Text =
+                @"Noted:
+病例组:数据列A=>G，A:PATIENT_ID B:SEX C:AGE
+对比组:数据列A=>G，A:PATIENT_ID B:SEX C:AGE D:TEST_NO E:REPORT_ITEM_NAME F:RESULT G:SOURCE
+保证两份数据文件中的表头与程序中的标注一致，否则会报错(卡住)";
+        }
+
+        private void btnExe_Click(object sender, EventArgs e)
+        {
+            this.rtbLogs.Text = "";
+            backgroundWorker1.RunWorkerAsync();
+        }
+           
+        private static string GetComparedData(DataTable dt, string gender, int age, string dtesxit)
+        {
+            DataRow[] drs = dt.Select("SEX = '" + gender + "' and AGE = " + age);
+            if (drs.Length > 0)
+            { 
+                DataTable dt0 = FilterData(drs.CopyToDataTable(), dtesxit);
+                if (dt0.Rows.Count == 0) goto Next;
+                int count = dt0.Rows.Count - 1;
+                string number = dt0.Rows[new Random().Next(0, count)]["PATIENT_ID"].ToString();
+                return number;
+            }
+            else
+            {
+                drs = dt.Select("SEX = '" + gender + "' and AGE <= " + age, "AGE DESC");
+                if (drs.Length > 0)
+                {
+                    DataTable dt0 = FilterData(drs.CopyToDataTable(), dtesxit);
+                    if (dt0.Rows.Count == 0) goto Next2;
+                    int count = dt0.Rows.Count - 1;
+                    string number = dt0.Rows[new Random().Next(0, count)]["PATIENT_ID"].ToString();
+                    return number;
+                }
+                else
+                {
+                    drs = dt.Select("SEX = '" + gender + "' and AGE >= " + age, "AGE ASC");
+                    {
+                        DataTable dt0 = FilterData(drs.CopyToDataTable(), dtesxit);
+                        if (dt0.Rows.Count == 0) return "";
+                        int count = dt0.Rows.Count - 1;
+                        string number = dt0.Rows[new Random().Next(0, count)]["PATIENT_ID"].ToString();
+                        return number;
+                    }
+                }
+            }
+
+            Next:
+            drs = dt.Select("SEX = '" + gender + "' and AGE <= " + age, "AGE DESC");
+            if (drs.Length > 0)
+            {
+                DataTable dt0 = FilterData(drs.CopyToDataTable(), dtesxit);
+                if (dt0.Rows.Count == 0) goto Next2;
+                int count = dt0.Rows.Count - 1;
+                string number = dt0.Rows[new Random().Next(0, count)]["PATIENT_ID"].ToString();
+                return number;
+            }
+
+            Next2:
+            drs = dt.Select("SEX = '" + gender + "' and AGE >= " + age, "AGE ASC");
+            if (drs.Length > 0)
+            {
+                DataTable dt0 = FilterData(drs.CopyToDataTable(), dtesxit);
+                if (dt0.Rows.Count == 0) return "";
+                int count = dt0.Rows.Count - 1;
+                string number = dt0.Rows[new Random().Next(0, count)]["PATIENT_ID"].ToString();
+                return number;
+            }
+
+            return "";
+        }
+
+        private static DataTable FilterData(DataTable oldTable, string e) 
+        {
+            DataTable newTable = oldTable.Clone();
+            foreach (DataRow dr in oldTable.Rows) 
+            {
+                if (!e.Contains(dr["PATIENT_ID"].ToString()))
+                    newTable.Rows.Add(dr.ItemArray);
+            }
+            newTable.AcceptChanges();
+            return newTable;
+        }
+        private void tbxExcelD_TextChanged(object sender, EventArgs e)
+        {
+            if (tbxExcelD.Text != "")
+                exceld = tbxExcelD.Text;
+        }
+
+        private void tbxExcelC_TextChanged(object sender, EventArgs e)
+        {
+            if (tbxExcelC.Text != "")
+                excelc = tbxExcelC.Text;
+        }
+
+        private void btnOpen1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dialog = new OpenFileDialog();
+            dialog.Title = "Select file";
+            dialog.InitialDirectory = ".\\";
+            dialog.Filter = "xls files (*.*)|*.xlsx";
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                this.tbxExcelD.Text = dialog.FileName;
+                exceld = dialog.FileName;
+            }
+        }
+
+        private void btnOpen2_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dialog = new OpenFileDialog();
+            dialog.Title = "Select file";
+            dialog.InitialDirectory = ".\\";
+            dialog.Filter = "xls files (*.*)|*.xlsx";
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                this.tbxExcelC.Text = dialog.FileName;
+                excelc = dialog.FileName;
+            }
+        }
+
+        private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+        {
+            string existeddata = "";
+            if (exceld != "" && excelc != "")
+            {
+                string urlDisease = exceld; //病例组的文件
+                string urlCompared = excelc;  //对照组的数据文件
+                this.backgroundWorker1.ReportProgress(0, "[" + DateTime.Now.ToString() + "]" + "病例组与对比组文件输入程序中..." + Environment.NewLine);
+                Workbook workbook; Workbook workbookC; Cells cellsC; DataTable dtExcelC;
+                try
+                {
+                    workbookC = new Workbook(urlCompared);
+                    cellsC = workbookC.Worksheets[0].Cells;//取对应的工作表.第几个工作表
+                    dtExcelC = cellsC.ExportDataTableAsString(0, 0, cellsC.MaxDataRow + 1, cellsC.MaxDataColumn + 1, true);//将Excel表格转换成DataTable进行处理
+                }
+                catch (Exception ex)
+                {
+                    this.backgroundWorker1.ReportProgress(0, "[" + DateTime.Now.ToString() + "]" + "对比组文件路径不正确。请检查后再执行。" + ex.Message + Environment.NewLine); return;
+                }
+                try { workbook = new Workbook(urlDisease); }
+                catch (Exception ex)
+                {
+                    this.backgroundWorker1.ReportProgress(0, "[" + DateTime.Now.ToString() + "]" + "病例组文件路径不正确。请检查后再执行。" + ex.Message + Environment.NewLine); return;
+                }
+                foreach (Worksheet sheet in workbook.Worksheets)
+                {
+                    this.backgroundWorker1.ReportProgress(25, "[" + DateTime.Now.ToString() + "]" + "导入成功开始对比操作..." + Environment.NewLine);
+                    Cells cells = sheet.Cells;
+                    var styles = cells["A1"].GetStyle();
+                    cells["H1"].PutValue("ControlGroup:");
+                    cells["I1"].PutValue("PATIENT_ID"); 
+                    cells["J1"].PutValue("SEX"); 
+                    cells["K1"].PutValue("AGE"); 
+                    cells["L1"].PutValue("TEST_NO"); 
+                    cells["M1"].PutValue("REPORT_ITEM_NAME");
+                    cells["N1"].PutValue("RESULT");
+                    cells["O1"].PutValue("SOURCE");
+
+                    for (int i = 2; i < cells.Rows.Count + 1; i++)//从第二行开始处理数据
+                    {
+                        if (cells["A" + i.ToString()] != null && cells["A" + i.ToString()].Value != null)
+                        {
+                            if (!String.IsNullOrEmpty(cells["A" + i.ToString()].Value.ToString())) //患者识别号不为空
+                            {
+                                string comparedNum = "";
+                                try
+                                {
+                                    comparedNum = GetComparedData(dtExcelC, cells["B" + i.ToString()].Value.ToString(), int.Parse(cells["C" + i.ToString()].Value.ToString().Replace("岁", "")), existeddata);
+                                }
+                                catch (Exception ex)
+                                { 
+                                    this.backgroundWorker1.ReportProgress(30, "[" + DateTime.Now.ToString() + "]" + "病例组字段不正确，请检查后再执行。" + ex.Message + Environment.NewLine); return; 
+                                }
+                                if (comparedNum != "")
+                                {
+                                    existeddata += comparedNum + "-";
+                                    EnumerableRowCollection<DataRow> query = from person in dtExcelC.AsEnumerable()
+                                                                             where person.Field<string>("PATIENT_ID") == comparedNum
+                                                                             select person;
+                                    DataTable newDT = query.AsDataView().ToTable();
+                                    if (newDT.Rows.Count > 0)
+                                    {
+                                        try
+                                        {
+                                            cells["I" + i.ToString()].PutValue(newDT.Rows[0]["PATIENT_ID"].ToString());
+                                            cells["J" + i.ToString()].PutValue(newDT.Rows[0]["SEX"].ToString());
+                                            cells["K" + i.ToString()].PutValue(newDT.Rows[0]["AGE"].ToString());
+                                            cells["L" + i.ToString()].PutValue(newDT.Rows[0]["TEST_NO"].ToString());
+                                            cells["M" + i.ToString()].PutValue(newDT.Rows[0]["REPORT_ITEM_NAME"].ToString());
+                                            cells["N" + i.ToString()].PutValue(newDT.Rows[0]["RESULT"].ToString());
+                                            cells["O" + i.ToString()].PutValue(newDT.Rows[0]["SOURCE"].ToString());
+                                            this.backgroundWorker1.ReportProgress(50, "[" + DateTime.Now.ToString() + "]" + "编号：" + cells["A" + i.ToString()].Value.ToString() + "查询到对照组编号：" + newDT.Rows[0]["PATIENT_ID"].ToString() + Environment.NewLine);
+                                        }
+                                        catch(Exception ex) 
+                                        {
+                                            this.backgroundWorker1.ReportProgress(30, "[" + DateTime.Now.ToString() + "]" + "对比组字段不正确，请检查后再执行。" + ex.Message + Environment.NewLine); return;
+                                        }                
+                                    }
+                                    else
+                                    {
+                                        this.backgroundWorker1.ReportProgress(50, "[" + DateTime.Now.ToString() + "]" + "编号：" + cells["A" + i.ToString()].Value.ToString() + "没有合适的匹配。" + Environment.NewLine);
+                                    }
+
+                                }
+                            }
+                            else
+                                this.backgroundWorker1.ReportProgress(50, "[" + DateTime.Now.ToString() + "]" + "PATIENT_ID为空。" + Environment.NewLine);
+                        }
+                    }
+                    for (int col = 0; col < cells.MaxColumn; col++)
+                    {
+                        sheet.AutoFitColumn(col, 0, cells.MaxRow);
+                    }
+                }
+                workbook.Save(urlDisease);
+                this.backgroundWorker1.ReportProgress(100, "[" + DateTime.Now.ToString() + "]" + "执行完毕。请检查文件输出。" + Environment.NewLine);
+            }
+        }
+
+        private void backgroundWorker1_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            string message = e.UserState.ToString();
+            this.rtbLogs.Text += message;
+        }
+
+        private void backgroundWorker1_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+
+        }
+
+        private void rtbLogs_TextChanged(object sender, EventArgs e)
+        {
+            rtbLogs.SelectionStart = rtbLogs.Text.Length;
+            rtbLogs.ScrollToCaret();
+        }
+    }
+}
